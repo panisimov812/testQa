0f02ae2442989b5839c35fa8e9cdb7c473b0485c
anisimovpk <anisimovpk@yoomoney.ru>
2024-09-14 22:40:31 +0300

    Добавил настройку для скриншота после теста

--- src/test/java/uiTests/TestBase.java
+++ src/test/java/uiTests/TestBase.java
@@ -1,13 +1,18 @@
 package uiTests;
 
+import io.qameta.allure.Attachment;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.OutputType;
 import uiSteps.TextBoxSteps;
 
 import static com.codeborne.selenide.Selenide.*;
 
 import static io.qameta.allure.Allure.step;
+
+import static com.codeborne.selenide.Selenide.screenshot;
 
 public class TestBase {
 
@@ -16,9 +21,21 @@ public class TestBase {
     static void setUp(){
 
         Configuration.baseUrl = "https://demoqa.com";
-
+        Configuration.browser = "Firefox";
         step("открывает textbox", () -> {
             open("/text-box");
         });
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // Сохранение скриншота после каждого теста
+        screenshot("screenshot_after_test");
+    }
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshotToAllure() {
+        // Сохраняем скриншот и возвращаем его как массив байт для Allure
+        return screenshot(OutputType.BYTES);
     }
 }
